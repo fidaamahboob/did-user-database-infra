b9310cb07c6586e272486bcf41aa0f2f53491197
Fidaa Mahboob <fidaamahboob@Mac.lan>
2025-11-06 14:17:59 +0000

    Added output block to display bastion host public ip

--- infra/output.tf
+++ infra/output.tf
@@ -5,3 +5,7 @@ output "db_endpoint" {
 output "api_url" {
   value = "${aws_api_gateway_stage.stage.invoke_url}/data"
 }
+
+output "ec2_bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
